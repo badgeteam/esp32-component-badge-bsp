3686231045c8ea7dc8d343d2c83d483a2ddd144a
Renze Nicolai <renze@rnplus.nl>
2024-11-15 15:19:38 +0100

    Cleanup and add why2025 and mch2022 targets

---
+++ bsp/device.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "esp_err.h"
+
+// Badge BSP
+// Device information APIs
+
+/// @brief Get the name of the device as a string
+/// @details Returns the name as a null terminated string
+/// @param[out] output Pointer to buffer to which the name string gets copied
+/// @param[in] buffer_length Length of the output buffer
+/// @return ESP-IDF error code
+///          - ESP_OK if the output string fits in the buffer
+///          - ESP_ERR_INVALID_ARG if the output pointer is NULL
+esp_err_t bsp_device_get_name(char* output, uint8_t buffer_length);
+
+/// @brief Get the name of the manufacturer as a string
+/// @details Returns the name as a null terminated string
+/// @param[out] output Pointer to buffer to which the name string gets copied
+/// @param[in] buffer_length Length of the output buffer
+/// @return ESP-IDF error code
+///          - ESP_OK if the output string fits in the buffer
+///          - ESP_ERR_INVALID_ARG if the output pointer is NULL
+esp_err_t bsp_device_get_manufacturer(char* output, uint8_t buffer_length);
---
+++ targets/mch2022/badge_bsp_device.c
@@ -0,0 +1,28 @@
+// Board support package API: MCH2022 implementation
+// SPDX-FileCopyrightText: 2024 Nicolai Electronics
+// SPDX-License-Identifier: MIT
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include "bsp/device.h"
+#include "esp_err.h"
+
+static const char device_name[] = "MCH2022 badge";
+static const char device_manufacturer[] = "Badge.Team";
+
+esp_err_t bsp_device_get_name(char* output, uint8_t buffer_length) {
+    if (output == NULL) {
+        return ESP_ERR_INVALID_ARG;
+    }
+    strlcpy(output, device_name, buffer_length);
+    return ESP_OK;
+}
+
+esp_err_t bsp_device_get_manufacturer(char* output, uint8_t buffer_length) {
+    if (output == NULL) {
+        return ESP_ERR_INVALID_ARG;
+    }
+    strlcpy(output, device_manufacturer, buffer_length);
+    return ESP_OK;
+}

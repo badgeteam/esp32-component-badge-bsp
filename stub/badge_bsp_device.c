4cd2de5ff9a9a04916eb0031425e422b5bcae31d
Renze Nicolai <renze@rnplus.nl>
2024-11-16 21:40:23 +0100

    Initialize display and I2C bus in device initialization function

--- stub/badge_bsp_device.c
+++ stub/badge_bsp_device.c
@@ -11,6 +11,10 @@
 static const char device_name[] = "Generic board";
 static const char device_manufacturer[] = "Unknown";
 
+esp_err_t __attribute__((weak)) bsp_device_initialize(void) {
+    return ESP_OK;
+}
+
 esp_err_t __attribute__((weak)) bsp_device_get_name(char* output, uint8_t buffer_length) {
     if (output == NULL) {
         return ESP_ERR_INVALID_ARG;

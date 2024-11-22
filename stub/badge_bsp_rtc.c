b2847c2b91ce138f937d545efa67d5a6353a2e50
Renze Nicolai <renze@rnplus.nl>
2024-11-22 03:10:40 +0100

    Mark stub RTC functions as weak

--- stub/badge_bsp_rtc.c
+++ stub/badge_bsp_rtc.c
@@ -9,14 +9,14 @@
 
 static char const *TAG = "BSP RTC";
 
-esp_err_t bsp_rtc_get_time(uint32_t *value) {
+esp_err_t __attribute__((weak)) bsp_rtc_get_time(uint32_t *value) {
     return ESP_ERR_NOT_SUPPORTED;
 }
 
-esp_err_t bsp_rtc_set_time(uint32_t value) {
+esp_err_t __attribute__((weak)) bsp_rtc_set_time(uint32_t value) {
     return ESP_ERR_NOT_SUPPORTED;
 }
 
-esp_err_t bsp_rtc_update_time(void) {
+esp_err_t __attribute__((weak)) bsp_rtc_update_time(void) {
     return ESP_ERR_NOT_SUPPORTED;
 }

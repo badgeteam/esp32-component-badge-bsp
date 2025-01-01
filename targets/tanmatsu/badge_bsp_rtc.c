c17133207f069c8ca362c5561662c86d6edc1884
Renze Nicolai <renze@rnplus.nl>
2025-01-01 17:53:57 +0100

    Add functions for RTC alarm

--- targets/tanmatsu/badge_bsp_rtc.c
+++ targets/tanmatsu/badge_bsp_rtc.c
@@ -17,24 +17,38 @@ static char const *TAG = "BSP RTC";
 esp_err_t bsp_rtc_get_time(uint32_t *value) {
     tanmatsu_coprocessor_handle_t handle = NULL;
     ESP_RETURN_ON_ERROR(bsp_tanmatsu_coprocessor_get_handle(&handle), TAG, "Failed to get coprocessor handle");
-    ESP_RETURN_ON_ERROR(tanmatsu_coprocessor_get_real_time(handle, value), TAG, "Failed to get RTC value");
+    ESP_RETURN_ON_ERROR(tanmatsu_coprocessor_get_real_time(handle, value), TAG, "Failed to get RTC time");
     return ESP_OK;
 }
 
 esp_err_t bsp_rtc_set_time(uint32_t value) {
     tanmatsu_coprocessor_handle_t handle = NULL;
     ESP_RETURN_ON_ERROR(bsp_tanmatsu_coprocessor_get_handle(&handle), TAG, "Failed to get coprocessor handle");
-    ESP_RETURN_ON_ERROR(tanmatsu_coprocessor_set_real_time(handle, value), TAG, "Failed to set RTC value");
+    ESP_RETURN_ON_ERROR(tanmatsu_coprocessor_set_real_time(handle, value), TAG, "Failed to set RTC time");
     return ESP_OK;
 }
 
 esp_err_t bsp_rtc_update_time(void) {
     uint32_t value;
-    ESP_RETURN_ON_ERROR(bsp_rtc_get_time(&value), TAG, "Failed to get RTC value");
+    ESP_RETURN_ON_ERROR(bsp_rtc_get_time(&value), TAG, "Failed to get RTC time");
     struct timeval rtc_timeval = {
         .tv_sec  = value,
         .tv_usec = 0,
     };
     settimeofday(&rtc_timeval, NULL);
+    return ESP_OK;
+}
+
+esp_err_t bsp_rtc_get_alarm(uint32_t *value) {
+    tanmatsu_coprocessor_handle_t handle = NULL;
+    ESP_RETURN_ON_ERROR(bsp_tanmatsu_coprocessor_get_handle(&handle), TAG, "Failed to get coprocessor handle");
+    ESP_RETURN_ON_ERROR(tanmatsu_coprocessor_get_alarm_time(handle, value), TAG, "Failed to get alarm time");
+    return ESP_OK;
+}
+
+esp_err_t bsp_rtc_set_alarm(uint32_t value) {
+    tanmatsu_coprocessor_handle_t handle = NULL;
+    ESP_RETURN_ON_ERROR(bsp_tanmatsu_coprocessor_get_handle(&handle), TAG, "Failed to get coprocessor handle");
+    ESP_RETURN_ON_ERROR(tanmatsu_coprocessor_set_alarm_time(handle, value), TAG, "Failed to set alarm time");
     return ESP_OK;
 }

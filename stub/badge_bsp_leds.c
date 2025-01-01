3f23891e039ed9ce2159247475150b5059560ba6
Renze Nicolai <renze@rnplus.nl>
2025-01-01 17:53:36 +0100

    Add API for controlling LEDs

---
+++ stub/badge_bsp_leds.c
@@ -0,0 +1,16 @@
+// Board support package API: Generic stub implementation
+// SPDX-FileCopyrightText: 2024 Nicolai Electronics
+// SPDX-License-Identifier: MIT
+
+#include "bsp/leds.h"
+#include "esp_err.h"
+
+#include <stdint.h>
+
+esp_err_t __attribute__((weak)) bsp_leds_initialize(void) {
+    return ESP_ERR_NOT_SUPPORTED;
+}
+
+esp_err_t __attribute__((weak)) bsp_led_write(uint8_t *data, uint32_t length) {
+    return ESP_ERR_NOT_SUPPORTED;
+}

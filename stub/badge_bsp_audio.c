1deb9e7cc98c9ebfb48d951871710de068136c3d
Renze Nicolai <renze@rnplus.nl>
2025-02-19 22:58:43 +0100

    Add initial audio API functions, fix dependencies in CMakefile

--- stub/badge_bsp_audio.c
+++ stub/badge_bsp_audio.c
@@ -1,5 +1,5 @@
 // Board support package API: Generic stub implementation
-// SPDX-FileCopyrightText: 2024 Nicolai Electronics
+// SPDX-FileCopyrightText: 2025 Nicolai Electronics
 // SPDX-License-Identifier: MIT
 
 #include "bsp/audio.h"
@@ -16,5 +16,9 @@ esp_err_t __attribute__((weak)) bsp_audio_get_volume(float *out_percentage) {
 }
 
 esp_err_t __attribute__((weak)) bsp_audio_set_volume(float percentage) {
+    return ESP_ERR_NOT_SUPPORTED;
+}
+
+esp_err_t __attribute__((weak)) bsp_audio_set_amplifier(bool enable) {
     return ESP_ERR_NOT_SUPPORTED;
 }
--- targets/mch2022/target.cmake
+++
@@ -1,4 +0,0 @@
-set(BSP_REQUIRES
-    "espressif__esp_lcd_ili9341"
-    "badgeteam__mch2022-rp2040"
-)

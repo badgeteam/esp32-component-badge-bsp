3d25e27b64988544a79f189f83ef8a5cf476e0c6
Renze Nicolai <renze@rnplus.nl>
2025-05-10 13:46:55 +0200

    Add support for ESP32-P4 function EV board with LT8912B DSI to HDMI converter

---
+++ targets/esp32-p4-function-ev-board-lt8912b/devkit_hardware.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Display
+#define BSP_LCD_RESET_PIN      (21)
+#define BSP_LCD_PWM_PIN        (22)
+#define BSP_DSI_LDO_CHAN       (3)
+#define BSP_DSI_LDO_VOLTAGE_MV (2500)
+
+// I2C bus
+#define BSP_I2C_INTERNAL_BUS (0)
+#define BSP_I2C_INTERNAL_SDA_PIN (7)
+#define BSP_I2C_INTERNAL_SCL_PIN (8)
+

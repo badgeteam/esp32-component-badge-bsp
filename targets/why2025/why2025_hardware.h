d8769aea730a788ad2b3af9d9e81bdd694205fbd
Senna Hijlkema <Senna.Hijlkema@gmail.com>
2026-07-21 15:29:02 +0200

    Keyboard now works as well
    Keyboard events are showing up as do navigation, I was to lazy to fully sort the enum and switch case but all buttons seem to work

--- targets/why2025/why2025_hardware.h
+++ targets/why2025/why2025_hardware.h
@@ -10,6 +10,13 @@
 #define BSP_I2C_SDA_PIN 18
 #define BSP_I2C_SCL_PIN 20
 
+
+#define BSP_KBD_INT       2
+#define BSP_KBD_SCL       BSP_I2C_SCL_PIN
+#define BSP_KBD_SDA       BSP_I2C_SDA_PIN
+#define BSP_KBD_RST       -1
+
+
 // MIPI DSI display
 #define BSP_DSI_LDO_CHAN       3
 #define BSP_DSI_LDO_VOLTAGE_MV 2500
